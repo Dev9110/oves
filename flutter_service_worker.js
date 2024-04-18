acb43ea82df1e0a99ddc0e3a3a80a45ed96b218c
THEDEVHOME\dev <viyasandev04@gmail.com>
2024-04-19 02:03:20 +0800

    new build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"404.html": "0a27a4163254fc8fce870c8cc3a3f94f",
+<<<<<<< HEAD
+"assets/AssetManifest.bin": "64a232c4f4a05879996902bd650d675a",
+"assets/AssetManifest.bin.json": "1a6717f46ec0eb03c27beca220171b8e",
+"assets/AssetManifest.json": "4bfb4f94c704468455f7f6726df6d73c",
+=======
 "assets/AssetManifest.bin": "b2ee853339386726d155c03e4cc44633",
 "assets/AssetManifest.bin.json": "8c7a7b8468e0c15d4a653d08e3432127",
 "assets/AssetManifest.json": "395040b92b53d4ac1ef58ab75e47c001",
+>>>>>>> 7782c0d14587f02a90686fc097d80dd8a2bd993f
 "assets/FontManifest.json": "ee155429dad8edc65732328ce335afc7",
 "assets/fonts/MaterialIcons-Regular.otf": "5cd8d957f534cf0b63f94fc21bd9052f",
 "assets/fonts/Montserrat/Montserrat-Black.ttf": "27e3649bab7c62fa21b8837c4842e40e",
@@ -29,6 +35,20 @@ const RESOURCES = {"404.html": "0a27a4163254fc8fce870c8cc3a3f94f",
 "assets/fonts/Montserrat/Montserrat-ThinItalic.ttf": "3c2b290f95cd5b33c3ead2911064a2ab",
 "assets/images/approved.png": "5b6e38df552bf66ebc621c9994fe6782",
 "assets/images/auth_bg.png": "c5dae8f0858df9fbb742683ec60bf597",
+<<<<<<< HEAD
+"assets/images/bus_bg.png": "112fa5db3b4f300a1e3ff1100dda9a5c",
+"assets/images/jpkk.png": "64816a6fdb2b4b472bab104642171b7f",
+"assets/images/leading_logo.png": "a667488e2ca74d1972664faffb21f3c4",
+"assets/images/logo_app.png": "83ddc5902b3421b80c953b3f0d3560ac",
+"assets/images/logo_transparent.png": "8cad321f1ea162de17fb4dd49a5d6a99",
+"assets/images/psp_logo.png": "f6d4021f1a92377093f1601ead108fe3",
+"assets/images/psp_logo_stroke.png": "65b0b55f73b7173f7ea733bca371cff1",
+"assets/images/rejected.png": "68caf81c221bdb80d4f2d26ab428cb85",
+"assets/images/user_registration_vector.png": "bc1c3f575d3870cec4f8b045c196575d",
+"assets/NOTICES": "6c747ac092a86491d18e1414d788032f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "ef6f28bc951b9f824cabd08660de76dc",
+"assets/packages/timezone/data/latest_all.tzf": "871b8008607c14f36bae2388a95fdc8c",
+=======
 "assets/images/jpkk.png": "64816a6fdb2b4b472bab104642171b7f",
 "assets/images/logo_app.png": "83ddc5902b3421b80c953b3f0d3560ac",
 "assets/images/psp_logo.png": "f6d4021f1a92377093f1601ead108fe3",
@@ -36,6 +56,7 @@ const RESOURCES = {"404.html": "0a27a4163254fc8fce870c8cc3a3f94f",
 "assets/images/user_registration_vector.png": "bc1c3f575d3870cec4f8b045c196575d",
 "assets/NOTICES": "5281e92c843315aa1aa8e2640a18e023",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "ef6f28bc951b9f824cabd08660de76dc",
+>>>>>>> 7782c0d14587f02a90686fc097d80dd8a2bd993f
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/canvaskit.js.symbols": "38cba9233b92472a36ff011dc21c2c9f",
@@ -54,9 +75,15 @@ const RESOURCES = {"404.html": "0a27a4163254fc8fce870c8cc3a3f94f",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+<<<<<<< HEAD
+"index.html": "44c48e8457f2ef24811add061e782606",
+"/": "44c48e8457f2ef24811add061e782606",
+"main.dart.js": "38ba39d9b7ee724e18ff3093e5f0bc60",
+=======
 "index.html": "5f0962fd5183cda37a0eb7968e2612fd",
 "/": "5f0962fd5183cda37a0eb7968e2612fd",
 "main.dart.js": "f00518e82d2b1cda11fc81a0b632f28c",
+>>>>>>> 7782c0d14587f02a90686fc097d80dd8a2bd993f
 "manifest.json": "1aadb5bd533aee664eb8b4464518c973",
 "version.json": "ba8b42f924efce86a4b18579bd7d1381"};
 // The application shell files that are downloaded before a service worker can
